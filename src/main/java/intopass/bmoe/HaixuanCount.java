9093cffc003c5b3930760bc3b64f2c114407695d
fuhaiwei <fuhaiwei@126.com>
2015-12-23 10:25:49 +0800

    add haixuan_16 count

--- src/main/java/intopass/bmoe/HaixuanCount.java
+++ src/main/java/intopass/bmoe/HaixuanCount.java
@@ -16,18 +16,26 @@ public class HaixuanCount {
     public static void main(String[] args) {
         List<Person> haixuan = Spider.get_persons(LocalDate.of(2015, 10, 31), LocalDate.of(2015, 12, 6));
         List<Person> benzhan_32 = Spider.get_persons(LocalDate.of(2015, 12, 20), LocalDate.of(2015, 12, 24));
+        List<Person> benzhan_16 = benzhan_32.stream().filter(p -> p.rank == 1).collect(toList());
 
         List<Person> haixuan_32 = benzhan_32.stream()
                 .map(p -> find_by_rid(haixuan, p.rid))
                 .collect(toList());
+        List<Person> haixuan_16 = benzhan_16.stream()
+                .map(p -> find_by_rid(haixuan, p.rid))
+                .collect(toList());
 
-        print_date("海选晋级32强日期分布情况", haixuan_32);
+        print_date("海选晋级32强日期分布情况 女子组", haixuan_32, 0);
+        print_date("海选晋级32强日期分布情况 男子组", haixuan_32, 1);
+
+        print_date("海选晋级16强日期分布情况 女子组", haixuan_16, 0);
+        print_date("海选晋级16强日期分布情况 男子组", haixuan_16, 1);
     }
 
-    private static void print_date(String title, List<Person> persons) {
+    private static void print_date(String title, List<Person> persons, int sex) {
         System.out.println(title);
         Map<String, List<Person>> collect = persons.stream()
-                .filter(p -> p.sex == 0)
+                .filter(p -> p.sex == sex)
                 .collect(groupingBy(p -> p.date));
         collect.keySet().stream()
                 .sorted()
@@ -37,7 +45,7 @@ public class HaixuanCount {
                             .sorted()
                             .map(p -> p.name + "(" + p.rank + ")")
                             .collect(joining(" "));
-                    System.out.printf("%s: %d人 %s%n",
+                    System.out.printf("%s: (%d人) %s%n",
                             date.substring(5), dates.size(), names);
                 });
     }
